7680c419f6490c35edc8c908d5ef19e3b6cc1eda
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-04-02 17:02:49 -0300

    Update Faça um programa que solicita e armazena seis números inteiros em uma matriz 2 x 3, denominada de A, e solicita e armazena seis números inteiros em outra matriz 2 x 3, denominada de B. Em seguida, exiba a soma dessas matrizes..cpp

--- Faça um programa que solicita e armazena seis números inteiros em uma matriz 2 x 3, denominada de A, e solicita e armazena seis números inteiros em outra matriz 2 x 3, denominada de B. Em seguida, exiba a soma dessas matrizes..cpp
+++ Faça um programa que solicita e armazena seis números inteiros em uma matriz 2 x 3, denominada de A, e solicita e armazena seis números inteiros em outra matriz 2 x 3, denominada de B. Em seguida, exiba a soma dessas matrizes..cpp
@@ -3,31 +3,31 @@
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
-	int a[2][3], b[2][3], x = 1, i, j, s[2][3];
+	int a[2][3], b[2][3], x = 1, i, j;
+	//Laço para receber os valores de A
+	printf("Matriz A:\n");
 	for(i = 0; i < 2; i++){
 		for(j = 0; j < 3; j++){
 			printf("Digite o %iº valor: ", x++);
 			scanf("%i", &a[i][j]);
 		}
 	}
+	//Laço para receber os valores de B
+    printf("\n\nMatriz B:\n");
+	x = 1;
 	for(i = 0; i < 2; i++){
 		for(j = 0; j < 3; j++){
 			printf("Digite o %iº valor: ", x++);
 			scanf("%i", &b[i][j]);
 		}
 	}
-	printf("\nSoma entre os valores informados:\n");
+	//Laço para exibir a soma das matrizes A + B
+	printf("\nSoma das matrizes A e B:\n");
 	for(i = 0; i < 2; i++){
 		for(j = 0; j < 3; j++){
-			s[i][j]= a[i][j] + b[i][j];
-		}
-	}
-	for(i = 0; i < 2; i++){
-		for(j = 0; j < 3; j++){
-			printf("%i ", s[i][j]);
+			printf("%i\t", a[i][j] + b[i][j]);
 		}
 		printf("\n");
 	}
-	
 	return 0;
 }
