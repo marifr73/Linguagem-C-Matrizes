4b1704fea132f2fa55e9b8ebeeacd0833d1ede9e
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-04-02 17:14:04 -0300

    Update Elabore programa que solicita e armazena oito números reais em uma matriz 4 × 2, denominada de A. Construa outra matriz, denominada de B, do mesmo tipo e tamanho de A, e com os mesmos elementos armazenados em A, porém de forma invertida. Ou seja, o primeiro elemento de A passa a ser o último de B, o segundo elemento de A passa a ser o penúltimo de B e assim por diante. Ao final, o programa deve exibir o conteúdo de A e B.cpp

--- Elabore programa que solicita e armazena oito números reais em uma matriz 4 × 2, denominada de A. Construa outra matriz, denominada de B, do mesmo tipo e tamanho de A, e com os mesmos elementos armazenados em A, porém de forma invertida. Ou seja, o primeiro elemento de A passa a ser o último de B, o segundo elemento de A passa a ser o penúltimo de B e assim por diante. Ao final, o programa deve exibir o conteúdo de A e B.cpp
+++ Elabore programa que solicita e armazena oito números reais em uma matriz 4 × 2, denominada de A. Construa outra matriz, denominada de B, do mesmo tipo e tamanho de A, e com os mesmos elementos armazenados em A, porém de forma invertida. Ou seja, o primeiro elemento de A passa a ser o último de B, o segundo elemento de A passa a ser o penúltimo de B e assim por diante. Ao final, o programa deve exibir o conteúdo de A e B.cpp
@@ -3,20 +3,22 @@
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
-	float A[4][2]; 
-    float B[4][2]; 
-    int i, j, x = 1;
+	float A[4][2], B[4][2];
+    	int i, j, x = 1;
+	//Informar o valor de A
   	for(i = 0; i < 4; i++){
 		for(j = 0; j < 2; j++){
 			printf("Digite o %iº valor: ", x++);
 			scanf("%f", &A[i][j]);
 		}
 	}
+	//Inverter o Valor de A para B
 	for(i = 0; i < 4; i++){
         	for(j = 0; j < 2; j++) {
             B[i][j] = A[3 - i][1 - j];
         	}
     	}
+	//Imprimir a Matriz A
 	printf("\nMatriz A:\n");
     for (i = 0; i < 4; i++) {
         for (j = 0; j < 2; j++) {
@@ -24,10 +26,11 @@ int main(){
         }
         printf("\n");
     }
-    printf("\nMatriz B:\n");
-    for (i = 0; i < 4; i++) {
-        for (j = 0; j < 2; j++) {
-            printf("%.2f ", B[i][j]);
+	//Imprimir a Matriz B
+    	printf("\nMatriz B:\n");
+    	for (i = 0; i < 4; i++) {
+        	for (j = 0; j < 2; j++) {
+            		printf("%.2f ", B[i][j]);
         }
         printf("\n");
     }
