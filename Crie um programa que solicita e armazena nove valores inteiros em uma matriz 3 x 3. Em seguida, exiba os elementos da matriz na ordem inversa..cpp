96006fbd86ff4005382feafe061214c9b57ca106
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-04-02 17:00:54 -0300

    Update Crie um programa que solicita e armazena nove valores inteiros em uma matriz 3 x 3. Em seguida, exiba os elementos da matriz na ordem inversa..cpp

--- Crie um programa que solicita e armazena nove valores inteiros em uma matriz 3 x 3. Em seguida, exiba os elementos da matriz na ordem inversa..cpp
+++ Crie um programa que solicita e armazena nove valores inteiros em uma matriz 3 x 3. Em seguida, exiba os elementos da matriz na ordem inversa..cpp
@@ -4,12 +4,14 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int i, j, x = 1, mat[3][3];
+	//Laço para receber os valores de mat
 	for(i = 0; i < 3; i++){
 		for(j = 0; j < 3; j++){
 			printf("Digite o %iº valor: ", x++);
 			scanf("%i", &mat[i][j]);
 		}
 	}
+	//Laço para exibir os valores na ordem inversa
 	printf("\nMATRIZ\n");
 	for(i = 2; i >= 0; i--){
 		for(j = 2; j >= 0; j--){
