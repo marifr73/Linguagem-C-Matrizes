cbc22704c334c89801b4d55300b7683c812c8c32
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-04-02 13:14:34 -0300

    Update Elabore um programa que solicita e armazena o nome e três notas de cinco alunos em uma matriz 5 x 3. Por fim, o programa deve exibir uma mensagem com o nome e a média aritmética de cada aluno..cpp

--- Elabore um programa que solicita e armazena o nome e três notas de cinco alunos em uma matriz 5 x 3. Por fim, o programa deve exibir uma mensagem com o nome e a média aritmética de cada aluno..cpp
+++ Elabore um programa que solicita e armazena o nome e três notas de cinco alunos em uma matriz 5 x 3. Por fim, o programa deve exibir uma mensagem com o nome e a média aritmética de cada aluno..cpp
@@ -7,24 +7,24 @@ int main(){
 	float nota[5][3], m[5];
 	int i, j;
 	for(i = 0; i < 5; i++){
-        printf("Digite o nome do %iº aluno: ", i+1);
-        scanf("%s", n[i]);
-        for(j = 0; j < 3; j++){
-            printf("Digite a %iª nota do aluno %s: ", j+1, n[i]);
-            scanf("%f", &nota[i][j]);
-        }
-         printf("\n");
-    }
+        	printf("Digite o nome do %iº aluno: ", i+1);
+        	scanf("%s", n[i]);
+        	for(j = 0; j < 3; j++){
+			printf("Digite a %iª nota do aluno %s: ", j+1, n[i]);
+           	 	scanf("%f", &nota[i][j]);
+       		 }
+        	 printf("\n");
+   	 }
 	for(i = 0; i < 5; i++){
         m[i] = 0;
-        for(j = 0; j < 3; j++){
-            m[i] += nota[i][j];
-        }
-        m[i] /= 3;
-    }
-    printf("\n");
-    for(i = 0; i < 5; i++){
-        printf("A média do aluno %s é: %.2f\n", n[i], m[i]);
-    }
+        	for(j = 0; j < 3; j++){
+           		 m[i] += nota[i][j];
+        	}
+      		  m[i] /= 3;
+    	}
+   	 printf("\n");
+  	  for(i = 0; i < 5; i++){
+       		 printf("A média do aluno %s é: %.2f\n", n[i], m[i]);
+	    }
 	return 0;
 }
