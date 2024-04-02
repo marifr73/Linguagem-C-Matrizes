e1bc8e51948f6bea12f6c6e5d542cc62cd44c8e8
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-04-02 13:11:15 -0300

    Update and rename Lista de C 6 Questão 3.cpp to Elabore um programa que solicita e armazena o nome e três notas de cinco alunos em uma matriz 5 x 3. Por fim, o programa deve exibir uma mensagem com o nome e a média aritmética de cada aluno..cpp

--- Elabore um programa que solicita e armazena o nome e três notas de cinco alunos em uma matriz 5 x 3. Por fim, o programa deve exibir uma mensagem com o nome e a média aritmética de cada aluno..cpp
+++ Elabore um programa que solicita e armazena o nome e três notas de cinco alunos em uma matriz 5 x 3. Por fim, o programa deve exibir uma mensagem com o nome e a média aritmética de cada aluno..cpp
@@ -7,10 +7,10 @@ int main(){
 	float nota[5][3], m[5];
 	int i, j;
 	for(i = 0; i < 5; i++){
-        printf("Digite o nome do %iº aluno: ", i+1);
+        printf("Digite o nome do %iÂº aluno: ", i+1);
         scanf("%s", n[i]);
         for(j = 0; j < 3; j++){
-            printf("Digite a %iª nota do aluno %s: ", j+1, n[i]);
+            printf("Digite a %iÂª nota do aluno %s: ", j+1, n[i]);
             scanf("%f", &nota[i][j]);
         }
          printf("\n");
@@ -24,7 +24,7 @@ int main(){
     }
     printf("\n");
     for(i = 0; i < 5; i++){
-        printf("A média do aluno %s é: %.2f\n", n[i], m[i]);
+        printf("A mÃ©dia do aluno %s Ã©: %.2f\n", n[i], m[i]);
     }
 	return 0;
 }
