7bb687df4e28872127829a561855d4c55ef78fb3
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-04-16 11:17:28 -0300

    Update Elabore um programa que solicita e armazena o nome e três notas de cinco alunos em uma matriz 5 x 3. Por fim, o programa deve exibir uma mensagem com o nome e a média aritmética de cada aluno..c

--- Elabore um programa que solicita e armazena o nome e três notas de cinco alunos em uma matriz 5 x 3. Por fim, o programa deve exibir uma mensagem com o nome e a média aritmética de cada aluno..c
+++ Elabore um programa que solicita e armazena o nome e três notas de cinco alunos em uma matriz 5 x 3. Por fim, o programa deve exibir uma mensagem com o nome e a média aritmética de cada aluno..c
@@ -10,25 +10,15 @@ int main(){
 	for(i = 0; i < 5; i++){
         	printf("Digite o nome do %iº aluno: ", i+1);
         	scanf("%s", n[i]);
-		//Laço para receber as notas
+			//Laço para receber as notas
         	for(j = 0; j < 3; j++){
 			printf("Digite a %iª nota do aluno %s: ", j+1, n[i]);
            	 	scanf("%f", &nota[i][j]);
-       		 }
-        	 printf("\n");
-   	 }
-	//Laço para média
-	for(i = 0; i < 5; i++){
-        m[i] = 0;
-        	for(j = 0; j < 3; j++){
-           		m[i] += nota[i][j];
-        	}
-      		  m[i] /= 3;
-    	}
-   	  printf("\n");
-	//Laço para exibir a média e o nome dos alunos
-    for(i = 0; i < 5; i++){
-        printf("A média do aluno %s é: %.2f\n", n[i], m[i]);
+           	 	m[i] += nota[i][j];
+       		}
+       		//Divide a media aritimetica e exibe o nome e a média
+			m[i] /= 3;
+        	printf("\nA média do aluno %s é: %.2f\n\n", n[i], m[i]);
     }
 	return 0;
 }
